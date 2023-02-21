5c0cc023ef8f503e6cd77d277f64a04085a6303c
Mgarciaqu <124626393+Mgarciaqu@users.noreply.github.com>
2023-02-21 11:30:02 -0500

    ochpython

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Asiento:
 
-    def _init_(self, color, precio, registro):
+    def __init__(self, color, precio, registro):
         self.color=color
         self.precio=precio
         self.registro=registro
@@ -11,7 +11,7 @@ class Asiento:
 
 class Motor:
 
-    def _init_(self, numeroCilindros, tipo, registro):
+    def __init__(self, numeroCilindros, tipo, registro):
         self.numeroCilindros=numeroCilindros
         self.tipo=tipo
         self.registro=registro
@@ -25,7 +25,7 @@ class Motor:
 
 class Auto:
     cantidadCreados = 0
-    def _init_(self, modelo, precio,asientos,marca,motor,registro):
+    def __init__(self, modelo, precio,asientos,marca,motor,registro):
         self.modelo=modelo
         self.precio=precio
         self.asientos=asientos
